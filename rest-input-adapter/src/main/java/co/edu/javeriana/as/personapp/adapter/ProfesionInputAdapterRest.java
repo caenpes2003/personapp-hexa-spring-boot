177d956eb7a093293ed98a9667af5046f5835240
Camilo Peñuela <caenpes2003@gmail.com>
2026-05-24 23:59:49 -0500

    feat(profesion): implementar CRUD completo de Profesion en arquitectura hexagonal
    Se siguen las capas del template usando Persona como referencia.
    
    Capa application:
    - ProfessionInputPort y ProfessionOutputPort.
    - ProfessionUseCase con seleccion de adaptador via setPersintence,
      reutilizando el mismo patron de Persona (Maria por defecto via
      Qualifier en el constructor, cambio a Mongo en runtime).
    
    Capa output (Maria y Mongo, simetricas):
    - ProfesionRepositoryMaria extends JpaRepository.
    - ProfessionOutputAdapterMaria con save, delete, find, findById.
    - ProfesionRepositoryMongo extends MongoRepository.
    - ProfessionOutputAdapterMongo equivalente, capturando MongoWriteException.
    
    Capa input REST:
    - ProfesionRequest y ProfesionResponse.
    - ProfesionMapperRest con conversion ida y vuelta.
    - ProfesionInputAdapterRest con seleccion dinamica de adaptador y
      helper buildResponse que escoge el mapper Maria o Mongo segun la
      base seleccionada.
    - ProfesionControllerV1 expone CRUD completo: listar, buscar por id,
      crear, editar, eliminar y contar. Endpoints validados manualmente
      con curl en ambas bases.

---
+++ rest-input-adapter/src/main/java/co/edu/javeriana/as/personapp/adapter/ProfesionInputAdapterRest.java
@@ -0,0 +1,123 @@
+package co.edu.javeriana.as.personapp.adapter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import co.edu.javeriana.as.personapp.application.port.in.ProfessionInputPort;
+import co.edu.javeriana.as.personapp.application.port.out.ProfessionOutputPort;
+import co.edu.javeriana.as.personapp.application.usecase.ProfessionUseCase;
+import co.edu.javeriana.as.personapp.common.annotations.Adapter;
+import co.edu.javeriana.as.personapp.common.exceptions.InvalidOptionException;
+import co.edu.javeriana.as.personapp.common.exceptions.NoExistException;
+import co.edu.javeriana.as.personapp.common.setup.DatabaseOption;
+import co.edu.javeriana.as.personapp.domain.Profession;
+import co.edu.javeriana.as.personapp.mapper.ProfesionMapperRest;
+import co.edu.javeriana.as.personapp.model.request.ProfesionRequest;
+import co.edu.javeriana.as.personapp.model.response.ProfesionResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Adapter
+public class ProfesionInputAdapterRest {
+
+	@Autowired
+	@Qualifier("professionOutputAdapterMaria")
+	private ProfessionOutputPort professionOutputPortMaria;
+
+	@Autowired
+	@Qualifier("professionOutputAdapterMongo")
+	private ProfessionOutputPort professionOutputPortMongo;
+
+	@Autowired
+	private ProfesionMapperRest profesionMapperRest;
+
+	ProfessionInputPort professionInputPort;
+
+	private String setProfessionOutputPortInjection(String dbOption) throws InvalidOptionException {
+		if (dbOption.equalsIgnoreCase(DatabaseOption.MARIA.toString())) {
+			professionInputPort = new ProfessionUseCase(professionOutputPortMaria);
+			return DatabaseOption.MARIA.toString();
+		} else if (dbOption.equalsIgnoreCase(DatabaseOption.MONGO.toString())) {
+			professionInputPort = new ProfessionUseCase(professionOutputPortMongo);
+			return DatabaseOption.MONGO.toString();
+		}
+		throw new InvalidOptionException("Invalid database option: " + dbOption);
+	}
+
+	private ProfesionResponse buildResponse(Profession profession, String selectedDb) {
+		if (selectedDb.equalsIgnoreCase(DatabaseOption.MARIA.toString())) {
+			return profesionMapperRest.fromDomainToAdapterRestMaria(profession);
+		}
+		return profesionMapperRest.fromDomainToAdapterRestMongo(profession);
+	}
+
+	public List<ProfesionResponse> historial(String database) {
+		log.info("Into historial Profesion in Input Adapter");
+		try {
+			String selectedDb = setProfessionOutputPortInjection(database);
+			return professionInputPort.findAll().stream()
+					.map(p -> buildResponse(p, selectedDb))
+					.collect(Collectors.toList());
+		} catch (InvalidOptionException e) {
+			log.warn(e.getMessage());
+			return new ArrayList<>();
+		}
+	}
+
+	public ProfesionResponse buscarUna(String database, Integer id) {
+		try {
+			String selectedDb = setProfessionOutputPortInjection(database);
+			Profession profession = professionInputPort.findOne(id);
+			return buildResponse(profession, selectedDb);
+		} catch (InvalidOptionException | NoExistException e) {
+			log.warn(e.getMessage());
+			return null;
+		}
+	}
+
+	public ProfesionResponse crear(ProfesionRequest request) {
+		try {
+			String selectedDb = setProfessionOutputPortInjection(request.getDatabase());
+			Profession profession = professionInputPort.create(profesionMapperRest.fromAdapterToDomain(request));
+			return buildResponse(profession, selectedDb);
+		} catch (InvalidOptionException e) {
+			log.warn(e.getMessage());
+			return null;
+		}
+	}
+
+	public ProfesionResponse editar(Integer id, ProfesionRequest request) {
+		try {
+			String selectedDb = setProfessionOutputPortInjection(request.getDatabase());
+			Profession profession = professionInputPort.edit(id, profesionMapperRest.fromAdapterToDomain(request));
+			return buildResponse(profession, selectedDb);
+		} catch (InvalidOptionException | NoExistException e) {
+			log.warn(e.getMessage());
+			return null;
+		}
+	}
+
+	public Boolean eliminar(String database, Integer id) {
+		try {
+			setProfessionOutputPortInjection(database);
+			return professionInputPort.drop(id);
+		} catch (InvalidOptionException | NoExistException e) {
+			log.warn(e.getMessage());
+			return false;
+		}
+	}
+
+	public Integer contar(String database) {
+		try {
+			setProfessionOutputPortInjection(database);
+			return professionInputPort.count();
+		} catch (InvalidOptionException e) {
+			log.warn(e.getMessage());
+			return 0;
+		}
+	}
+}
---
+++ rest-input-adapter/src/main/java/co/edu/javeriana/as/personapp/controller/ProfesionControllerV1.java
@@ -0,0 +1,71 @@
+package co.edu.javeriana.as.personapp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.javeriana.as.personapp.adapter.ProfesionInputAdapterRest;
+import co.edu.javeriana.as.personapp.model.request.ProfesionRequest;
+import co.edu.javeriana.as.personapp.model.response.ProfesionResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/profesion")
+public class ProfesionControllerV1 {
+
+	@Autowired
+	private ProfesionInputAdapterRest profesionInputAdapterRest;
+
+	@ResponseBody
+	@GetMapping(path = "/{database}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<ProfesionResponse> listar(@PathVariable String database) {
+		log.info("GET /api/v1/profesion/{}", database);
+		return profesionInputAdapterRest.historial(database.toUpperCase());
+	}
+
+	@ResponseBody
+	@GetMapping(path = "/{database}/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ProfesionResponse buscar(@PathVariable String database, @PathVariable Integer id) {
+		log.info("GET /api/v1/profesion/{}/{}", database, id);
+		return profesionInputAdapterRest.buscarUna(database.toUpperCase(), id);
+	}
+
+	@ResponseBody
+	@PostMapping(path = "", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ProfesionResponse crear(@RequestBody ProfesionRequest request) {
+		log.info("POST /api/v1/profesion");
+		return profesionInputAdapterRest.crear(request);
+	}
+
+	@ResponseBody
+	@PutMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ProfesionResponse editar(@PathVariable Integer id, @RequestBody ProfesionRequest request) {
+		log.info("PUT /api/v1/profesion/{}", id);
+		return profesionInputAdapterRest.editar(id, request);
+	}
+
+	@ResponseBody
+	@DeleteMapping(path = "/{database}/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Boolean eliminar(@PathVariable String database, @PathVariable Integer id) {
+		log.info("DELETE /api/v1/profesion/{}/{}", database, id);
+		return profesionInputAdapterRest.eliminar(database.toUpperCase(), id);
+	}
+
+	@ResponseBody
+	@GetMapping(path = "/{database}/count/total", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Integer contar(@PathVariable String database) {
+		log.info("GET /api/v1/profesion/{}/count/total", database);
+		return profesionInputAdapterRest.contar(database.toUpperCase());
+	}
+}
